b97a185ebf9a1784c3ba84977aa0fd23634482af
RianVitor26 <rianvitorlhe@gmail.com>
2023-09-29 05:03:14 -0300

    feat: configurando o prisma e conectando com o mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './entities/users/users.module';
 import { CardsModule } from './entities/cards/cards.module';
 import { DeckModule } from './entities/deck/deck.module';
-import { AuthModule } from './entities/auth/auth.module';
+import { PrismaService } from './database/prisma.service';
 
 @Module({
-  imports: [UsersModule, CardsModule, DeckModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, CardsModule, DeckModule],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
