e1f0431ce3a455d1fa1b66c2a9e6ca053e938aba
RianVitor26 <rianvitorlhe@gmail.com>
2023-09-29 06:07:26 -0300

    feat: criando crud de usuários

--- src/entities/users/users.controller.ts
+++ src/entities/users/users.controller.ts
@@ -1,7 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Delete,
+  Put,
+  Body,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user-dto';
+import { UpdateUserDto } from './dto/update-user-dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+  @Post()
+  create(@Body() userDto: CreateUserDto) {
+    const createdUser = this.usersService.create(userDto);
+    return createdUser;
+  }
+
+  @Get()
+  readAll() {
+    const users = this.usersService.readAll();
+    return users;
+  }
+
+  @Get('/:id')
+  readById(@Param('id') id: string) {
+    const user = this.usersService.readById(id);
+    return user;
+  }
+
+  @Put('/:id')
+  update(@Param('id') id: string, @Body() userDto: UpdateUserDto) {
+    const updatedUser = this.usersService.update(id, userDto);
+    return updatedUser;
+  }
+
+  @Delete('/:id')
+  delete(@Param('id') id: string) {
+    this.usersService.delete(id);
+  }
 }
--- src/entities/users/users.service.ts
+++ src/entities/users/users.service.ts
@@ -1,4 +1,107 @@
-import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../database/prisma.service';
+import {
+  ConflictException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user-dto';
+import { UpdateUserDto } from './dto/update-user-dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(userDto: CreateUserDto) {
+    const { email, password } = userDto;
+
+    const existingUser = await this.prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (existingUser) {
+      throw new ConflictException('Email already exists');
+    }
+
+    const saltOrRounds = 10;
+    const salt = await bcrypt.genSalt(saltOrRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const createdUser = await this.prisma.user.create({
+      data: {
+        ...userDto,
+        password: hashedPassword,
+      },
+    });
+    return {
+      ...createdUser,
+      password: undefined,
+    };
+  }
+
+  async readAll() {
+    const users = await this.prisma.user.findMany();
+
+    if (!users || users.length === 0) {
+      throw new NotFoundException('No users found');
+    }
+    return users;
+  }
+
+  async readById(id: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException('No users found');
+    }
+    return user;
+  }
+
+  async update(id: string, userDto: UpdateUserDto) {
+    const existsUser = await this.prisma.user.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    if (!existsUser) {
+      throw new NotFoundException('No users found');
+    }
+
+    const updatedUser = await this.prisma.user.update({
+      where: {
+        id: Number(id),
+      },
+      data: userDto,
+    });
+
+    return updatedUser;
+  }
+
+  async delete(id: string) {
+    const existsUser = await this.prisma.user.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    if (!existsUser) {
+      throw new NotFoundException('No users found');
+    }
+
+    await this.prisma.user.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    return { statusCode: HttpStatus.NO_CONTENT };
+  }
+}
