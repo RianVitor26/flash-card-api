b2d7d42052250325c363d6fac3d69435a9c3a3f2
RianVitor26 <rianvitorlhe@gmail.com>
2023-10-06 21:29:56 -0300

    feat: adicionando métodos findOne, remove e update com try catch para cards

--- src/entities/cards/cards.service.ts
+++ src/entities/cards/cards.service.ts
@@ -7,66 +7,143 @@ export class CardsService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async create(cardDto: CreateCardDto, userId: number, deckId: number) {
-    const userExists = await this.prismaService.user.findUnique({
-      where: {
-        id: Number(userId),
-      },
-    });
+    try {
+      const userExists = await this.prismaService.user.findUnique({
+        where: {
+          id: Number(userId),
+        },
+      });
 
-    if (!userExists) {
-      throw new NotFoundException('User not found');
-    }
+      if (!userExists) {
+        throw new NotFoundException('User not found');
+      }
 
-    const deckExists = await this.prismaService.deck.findUnique({
-      where: {
-        id: Number(deckId),
-      },
-    });
-    if (!deckExists) {
-      throw new NotFoundException('Deck not found');
-    }
+      const deckExists = await this.prismaService.deck.findUnique({
+        where: {
+          id: Number(deckId),
+        },
+      });
 
-    const createdCard = await this.prismaService.card.create({
-      data: {
-        term: cardDto.term,
-        translation: cardDto.translation,
-        deck: {
-          connect: {
-            id: Number(deckId),
+      if (!deckExists) {
+        throw new NotFoundException('Deck not found');
+      }
+
+      const createdCard = await this.prismaService.card.create({
+        data: {
+          term: cardDto.term,
+          translation: cardDto.translation,
+          deck: {
+            connect: {
+              id: Number(deckId),
+            },
           },
         },
-      },
-    });
+      });
 
-    return createdCard;
+      return createdCard;
+    } catch (error) {
+      throw error;
+    }
   }
 
   async findAll(userId: number, deckId: number) {
-    const userExists = await this.prismaService.user.findUnique({
-      where: {
-        id: Number(userId),
-      },
-    });
+    try {
+      const userExists = await this.prismaService.user.findUnique({
+        where: {
+          id: Number(userId),
+        },
+      });
 
-    if (!userExists) {
-      throw new NotFoundException('User not found');
+      if (!userExists) {
+        throw new NotFoundException('User not found');
+      }
+
+      const deckExists = await this.prismaService.deck.findUnique({
+        where: {
+          id: Number(deckId),
+        },
+      });
+
+      if (!deckExists) {
+        throw new NotFoundException('Deck not found');
+      }
+
+      const cards = await this.prismaService.card.findMany({
+        where: {
+          deckId: Number(deckId),
+        },
+      });
+
+      return cards;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    const deckExists = await this.prismaService.deck.findUnique({
-      where: {
-        id: Number(deckId),
-      },
-    });
-    if (!deckExists) {
-      throw new NotFoundException('Deck not found');
+  async findOne(cardId: number) {
+    try {
+      const card = await this.prismaService.card.findUnique({
+        where: {
+          id: Number(cardId),
+        },
+      });
+
+      if (!card) {
+        throw new NotFoundException('Card not found');
+      }
+
+      return card;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    const cards = await this.prismaService.card.findMany({
-      where: {
-        deckId: Number(deckId),
-      },
-    });
+  async update(cardId: number, cardDto: CreateCardDto) {
+    try {
+      const cardExists = await this.prismaService.card.findUnique({
+        where: {
+          id: Number(cardId),
+        },
+      });
 
-    return cards;
+      if (!cardExists) {
+        throw new NotFoundException('Card not found');
+      }
+
+      const updatedCard = await this.prismaService.card.update({
+        where: {
+          id: Number(cardId),
+        },
+        data: cardDto,
+      });
+
+      return updatedCard;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async remove(cardId: number) {
+    try {
+      const cardExists = await this.prismaService.card.findUnique({
+        where: {
+          id: Number(cardId),
+        },
+      });
+
+      if (!cardExists) {
+        throw new NotFoundException('Card not found');
+      }
+
+      await this.prismaService.card.delete({
+        where: {
+          id: Number(cardId),
+        },
+      });
+
+      return { message: 'Card deleted' };
+    } catch (error) {
+      throw error;
+    }
   }
 }
--- src/entities/users/users.service.ts
+++ src/entities/users/users.service.ts
@@ -14,100 +14,120 @@ export class UsersService {
   constructor(private prisma: PrismaService) {}
 
   async create(userDto: CreateUserDto) {
-    const { email, password } = userDto;
+    try {
+      const { email, password } = userDto;
 
-    const existingUser = await this.prisma.user.findUnique({
-      where: {
-        email: email,
-      },
-    });
+      const existingUser = await this.prisma.user.findUnique({
+        where: {
+          email: email,
+        },
+      });
 
-    if (existingUser) {
-      throw new ConflictException('Email already exists');
+      if (existingUser) {
+        throw new ConflictException('Email already exists');
+      }
+
+      const saltOrRounds = 10;
+      const salt = await bcrypt.genSalt(saltOrRounds);
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      const createdUser = await this.prisma.user.create({
+        data: {
+          ...userDto,
+          password: hashedPassword,
+        },
+      });
+      return {
+        ...createdUser,
+        password: undefined,
+      };
+    } catch (error) {
+      throw error;
     }
-
-    const saltOrRounds = 10;
-    const salt = await bcrypt.genSalt(saltOrRounds);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    const createdUser = await this.prisma.user.create({
-      data: {
-        ...userDto,
-        password: hashedPassword,
-      },
-    });
-    return {
-      ...createdUser,
-      password: undefined,
-    };
   }
 
   async findAll() {
-    const users = await this.prisma.user.findMany();
+    try {
+      const users = await this.prisma.user.findMany();
 
-    if (!users || users.length === 0) {
-      throw new NotFoundException('No users found');
+      if (!users || users.length === 0) {
+        throw new NotFoundException('No users found');
+      }
+      return users.map((user) => ({
+        ...user,
+        password: undefined,
+      }));
+    } catch (error) {
+      throw error;
     }
-    return users.map((user) => ({
-      ...user,
-      password: undefined,
-    }));
   }
 
   async findOne(userId: number) {
-    const user = await this.prisma.user.findUnique({
-      where: {
-        id: Number(userId),
-      },
-    });
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          id: Number(userId),
+        },
+      });
 
-    if (!user) {
-      throw new NotFoundException('No users found');
+      if (!user) {
+        throw new NotFoundException('No users found');
+      }
+      return {
+        ...user,
+        password: undefined,
+      };
+    } catch (error) {
+      throw error;
     }
-    return {
-      ...user,
-      password: undefined,
-    };
   }
 
   async update(userId: number, userDto: UpdateUserDto) {
-    const existsUser = await this.prisma.user.findUnique({
-      where: {
-        id: Number(userId),
-      },
-    });
+    try {
+      const existsUser = await this.prisma.user.findUnique({
+        where: {
+          id: Number(userId),
+        },
+      });
 
-    if (!existsUser) {
-      throw new NotFoundException('No users found');
+      if (!existsUser) {
+        throw new NotFoundException('No users found');
+      }
+
+      const updatedUser = await this.prisma.user.update({
+        where: {
+          id: Number(userId),
+        },
+        data: userDto,
+      });
+
+      return updatedUser;
+    } catch (error) {
+      throw error;
     }
-
-    const updatedUser = await this.prisma.user.update({
-      where: {
-        id: Number(userId),
-      },
-      data: userDto,
-    });
-
-    return updatedUser;
   }
 
   async delete(userId: number) {
-    const existsUser = await this.prisma.user.findUnique({
-      where: {
-        id: Number(userId),
-      },
-    });
+    try {
+      const existsUser = await this.prisma.user.findUnique({
+        where: {
+          id: Number(userId),
+        },
+      });
 
-    if (!existsUser) {
-      throw new NotFoundException('No users found');
+      if (!existsUser) {
+        throw new NotFoundException('No users found');
+      }
+
+      await this.prisma.user.delete({
+        where: {
+          id: Number(userId),
+        },
+      });
+
+      return { statusCode: HttpStatus.NO_CONTENT };
+    } catch (error) {
+      throw error;
     }
-
-    await this.prisma.user.delete({
-      where: {
-        id: Number(userId),
-      },
-    });
-
-    return { statusCode: HttpStatus.NO_CONTENT };
   }
 }
