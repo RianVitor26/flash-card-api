19abe54c707a03a1d23e0c3dd9747611da1f95f8
RianVitor26 <rianvitorlhe@gmail.com>
2023-10-06 21:36:31 -0300

    chore: criando module auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { UsersModule } from './entities/users/users.module';
 import { CardsModule } from './entities/cards/cards.module';
 import { DecksModule } from './entities/decks/decks.module';
 import { PrismaService } from './database/prisma.service';
+import { AuthModule } from './entities/auth/auth.module';
 
 @Module({
-  imports: [UsersModule, CardsModule, DecksModule],
+  imports: [UsersModule, CardsModule, DecksModule, AuthModule],
   controllers: [],
   providers: [PrismaService],
 })
