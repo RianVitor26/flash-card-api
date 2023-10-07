9c752a9ed47cbe0991de654205aa29c8752bb94a
RianVitor26 <rianvitorlhe@gmail.com>
2023-10-06 21:26:27 -0300

    refac: inserindo try catch nas funções

--- src/entities/decks/decks.service.ts
+++ src/entities/decks/decks.service.ts
@@ -7,83 +7,103 @@ export class DecksService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async create(userId: number, deckDto: CreateDeckDto) {
-    const user = await this.prismaService.user.findUnique({
-      where: { id: Number(userId) },
-    });
+    try {
+      const user = await this.prismaService.user.findUnique({
+        where: { id: Number(userId) },
+      });
 
-    if (!user) {
-      throw new NotFoundException(`User with ID ${userId} not found.`);
+      if (!user) {
+        throw new NotFoundException(`User with ID ${userId} not found.`);
+      }
+
+      const createdDeck = await this.prismaService.deck.create({
+        data: {
+          ...deckDto,
+          ownerId: user.id,
+        },
+      });
+
+      return createdDeck;
+    } catch (error) {
+      throw error;
     }
-
-    const createdDeck = await this.prismaService.deck.create({
-      data: {
-        ...deckDto,
-        ownerId: user.id,
-      },
-    });
-
-    return createdDeck;
   }
 
   async findAll(userId: number) {
-    const decks = await this.prismaService.deck.findMany({
-      where: { ownerId: Number(userId) },
-      include: {
-        cards: true,
-      },
-    });
+    try {
+      const decks = await this.prismaService.deck.findMany({
+        where: { ownerId: Number(userId) },
+        include: {
+          cards: true,
+        },
+      });
 
-    if (!decks) {
-      throw new NotFoundException(`Decks not found.`);
+      if (!decks) {
+        throw new NotFoundException(`Decks not found.`);
+      }
+
+      return decks;
+    } catch (error) {
+      throw error;
     }
-
-    return decks;
   }
 
   async findOne(userId: number, deckId: number) {
-    const deck = await this.prismaService.deck.findUnique({
-      where: { id: Number(deckId), ownerId: Number(userId) },
-    });
+    try {
+      const deck = await this.prismaService.deck.findUnique({
+        where: { id: Number(deckId), ownerId: Number(userId) },
+      });
 
-    if (!deck) {
-      throw new NotFoundException(
-        `Deck with ID ${deckId} not found for User with ID ${userId}.`,
-      );
+      if (!deck) {
+        throw new NotFoundException(
+          `Deck with ID ${deckId} not found for User with ID ${userId}.`,
+        );
+      }
+
+      return deck;
+    } catch (error) {
+      throw error;
     }
-
-    return deck;
   }
 
   async update(userId: number, deckId: number, deckDto: CreateDeckDto) {
-    const existingDeck = await this.findOne(userId, deckId);
+    try {
+      const existingDeck = await this.findOne(userId, deckId);
 
-    if (existingDeck) {
-      const updatedDeck = await this.prismaService.deck.update({
-        where: { id: Number(deckId) },
-        data: deckDto,
-      });
+      if (existingDeck) {
+        const updatedDeck = await this.prismaService.deck.update({
+          where: { id: Number(deckId) },
+          data: deckDto,
+        });
 
-      return updatedDeck;
+        return updatedDeck;
+      }
+    } catch (error) {
+      throw error;
     }
   }
 
   async delete(userId: number, deckId: number) {
-    const existingDeck = await this.findOne(userId, deckId);
+    try {
+      const existingDeck = await this.findOne(userId, deckId);
 
-    if (existingDeck) {
-      const relatedCards = await this.prismaService.card.findMany({
-        where: { deckId: Number(deckId) },
-      });
-
-      if (relatedCards.length > 0) {
-        await this.prismaService.card.deleteMany({
+      if (existingDeck) {
+        const relatedCards = await this.prismaService.card.findMany({
           where: { deckId: Number(deckId) },
         });
-      }
 
-      await this.prismaService.deck.delete({
-        where: { id: Number(deckId) },
-      });
+        if (relatedCards.length > 0) {
+          await this.prismaService.card.deleteMany({
+            where: { deckId: Number(deckId) },
+          });
+        }
+
+        await this.prismaService.deck.delete({
+          where: { id: Number(deckId) },
+        });
+      }
+    } catch (error) {
+      throw error;
     }
   }
 }
