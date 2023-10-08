152612b910df4dbf3a90de09710c446cb7025f59
RianVitor26 <rianvitorlhe@gmail.com>
2023-10-08 00:40:46 -0300

    docs: documentando a entidade decks com swagger

--- src/entities/decks/decks.controller.ts
+++ src/entities/decks/decks.controller.ts
@@ -9,32 +9,63 @@ import {
 } from '@nestjs/common';
 import { DecksService } from './decks.service';
 import { CreateDeckDto } from './dto/create-deck-dto';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @ApiTags('Decks')
 @Controller('users/:userId/decks')
 export class DecksController {
   constructor(private readonly deckService: DecksService) {}
 
-  @ApiOperation({ summary: 'create a deck for a user' })
+  @ApiBody({ type: CreateDeckDto })
+  @ApiOperation({ summary: 'Create a new deck' })
+  @ApiResponse({ status: 201, description: 'Deck created successfully' })
   @Post()
   create(@Body() deckDto: CreateDeckDto, @Param('userId') userId: number) {
     const createdDeck = this.deckService.create(userId, deckDto);
     return createdDeck;
   }
 
+  @ApiOperation({ summary: 'Find all decks' })
+  
+  @ApiResponse({ status: 200, description: 'Decks found successfully' })
   @Get()
   findAll(@Param('userId') userId: number) {
     const decks = this.deckService.findAll(userId);
     return decks;
   }
 
+  @ApiOperation({ summary: 'Find one deck' })
+  @ApiResponse({ status: 200, description: 'Deck found successfully' })
+  @ApiParam({
+    name: 'userId',
+    description: 'User ID',
+  })
+  @ApiParam({
+    name: 'deckId',
+    description: 'Deck ID',
+  })
   @Get('/:deckId')
   findOne(@Param('userId') userId: number, @Param('deckId') deckId: number) {
     const deck = this.deckService.findOne(userId, deckId);
     return deck;
   }
 
+  @ApiOperation({ summary: 'Update one deck' })
+  @ApiResponse({ status: 200, description: 'Deck updated successfully' })
+  @ApiParam({
+    name: 'userId',
+    description: 'User ID',
+  })
+  @ApiParam({
+    name: 'deckId',
+    description: 'Deck ID',
+  })
   @Put('/:deckId')
   update(
     @Param('userId') userId: number,
@@ -45,6 +76,16 @@ export class DecksController {
     return updatedDeck;
   }
 
+  @ApiOperation({ summary: 'Delete one deck' })
+  @ApiResponse({ status: 204, description: 'Deck deleted successfully' })
+  @ApiParam({
+    name: 'userId',
+    description: 'User ID',
+  })
+  @ApiParam({
+    name: 'deckId',
+    description: 'Deck ID',
+  })
   @Delete('/:deckId')
   delete(@Param('userId') userId: number, @Param('deckId') deckId: number) {
     this.deckService.delete(userId, deckId);
--- src/entities/decks/dto/create-deck-dto.ts
+++ src/entities/decks/dto/create-deck-dto.ts
@@ -1,7 +1,14 @@
-export interface CreateDeckDto {
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateDeckDto {
+  @ApiProperty()
   id: number;
+  @ApiProperty()
   name: string;
+  @ApiProperty()
   description?: string;
+  @ApiProperty()
   category: string;
+  @ApiProperty()
   ownerId: number;
 }
